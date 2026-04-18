fdfb246f53cc782c31ee15963430cb31a2596e31
Husnain Rehman <husnainrehman86@gmail.com>
2026-04-18 01:53:27 -0400

    responiive the about section

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -18,32 +18,32 @@ export default function AboutUs({
                 {title}
                 </h2>
             </div>
-            <p className="text-[52px] leading-[1.2] text-[var(--primary)] font-[600] mt-[50px] mb-[35px]">
+            <p className="xl:text-[52px] md:text-[40px] text-[27px] leading-[1.2] text-[var(--primary)] font-[600] sm:mt-[50px] mt-[30px] mb-[35px]">
             {description}
             </p>
           </div>
-            <div className='flex gap-10'> 
-                <div className="w-[50%] pl-30 relative">
+            <div className='flex md:flex-row flex-col gap-10'> 
+                <div className="md:w-[50%] xl:pl-30 md:pl-20 pl-[55px] relative">
                     <svg className='absolute top-0 left-0' xmlns="http://www.w3.org/2000/svg" width="45" height="339" viewBox="0 0 45 339" fill="none">
                     <path d="M1 1V338C8.90805 305.345 20 295 44 284.711" stroke="#0F0E0E" stroke-width="2" stroke-linecap="round"/>
                     </svg>
-                    <div className="mb-20">
-                        <p className="text-[200px] mb-[50px] leading-none font-bold text-[#000]">
+                    <div className="lg:mb-20 mb-10">
+                        <p className="lg:text-[200px] text-[140px] lg:mb-[50px] mb-[30px] leading-none font-bold text-[#000]">
                          {experienceText}
                         </p>
-                        <p className='text-[70px] leading-none font-bold text-[var(--primary)]'>Years Experience</p>
+                        <p className='xl:text-[70px] lg:text-[60px] md:text-[43px] sm:text-[38px] text-[34px] leading-none font-bold text-[var(--primary)]'>Years Experience</p>
                     </div>
                     {link?.label && link?.href ? (
                         <a
                         href={link.href}
-                        className="inline-flex items-center justify-center rounded-tl-none rounded-tr-xl rounded-bl-xl rounded-br-none bg-[var(--primary)] p-2 max-w-[150px] h-[70px] w-full text-[14px] font-semibold text-white transition hover:opacity-95"
+                        className="inline-flex items-center justify-center rounded-tl-none rounded-tr-xl rounded-bl-xl rounded-br-none bg-[var(--primary)] p-2 max-w-[150px] sm:h-[70px] h-[50px] w-full text-[14px] font-semibold text-white transition hover:opacity-95"
                         >
                         {link.label}
                         <span className="ml-2 text-white">↗</span>
                         </a>
                     ) : null}
                 </div>
-                <div className="relative w-[50%] overflow-hidden  bg-[var(--primary)] pt-3  rounded-tl-none rounded-tr-[40px] rounded-bl-[40px] rounded-br-none">
+                <div className="relative h-fit md:w-[50%] overflow-hidden  bg-[var(--primary)] pt-3  rounded-tl-none rounded-tr-[40px] rounded-bl-[40px] rounded-br-none">
                 {image ? (
                 <div className=" overflow-hidden ">
                     <img
